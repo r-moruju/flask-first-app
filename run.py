a82e9f250d33151639c2b5420d62dedefdc6d54c
R Moruju <morujurazvan@gmail.com>
2022-12-04 16:43:35 +0000

    Add contact form

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import os
 import json
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app  = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/contact",  methods = ["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
+    return render_template("contact.html")
 
 @app.route("/about")
 def about():
